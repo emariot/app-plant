441a5a4ba3fc411106a9ea364de61f59812f4595
Eduardo <eduardo.mariot@gmail.com>
2019-05-02 01:14:22 -0300

    Screens SignUp

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,14 +1,131 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  ActivityIndicator,
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet
+} from "react-native";
+
+import { Block, Text, Input, Button } from "../components";
+import { theme } from "../constants";
 
 export default class SignUp extends Component {
+  state = {
+    email: null,
+    username: null,
+    password: null,
+    errors: [],
+    loading: false
+  };
+
+  handleSignUp() {
+    const { navigation } = this.props;
+    const { email, username, password } = this.state;
+    const errors = [];
+
+    Keyboard.dismiss();
+
+    this.setState({ loading: true });
+
+    // check with backend API or with some static data
+    if (!email) errors.push("email");
+    if (!username) errors.push("username");
+    if (!password) errors.push("password");
+
+    this.setState({ errors, loading: false });
+
+    if (!errors.length) {
+      Alert.alert(
+        "Success!",
+        "Your account has been created.",
+        [
+          {
+            text: "Continue",
+            onPress: () => {
+              navigation.navigate("Browse");
+            }
+          }
+        ],
+        { cancelable: false }
+      );
+    }
+  }
   render() {
+    const { navigation } = this.props;
+    const { errors, loading } = this.state;
+    const hasErrors = key => (errors.includes(key) ? styles.hasErrors : null);
     return (
-      <View>
-        <Text> SignUp </Text>
-      </View>
+      <KeyboardAvoidingView style={styles.SignUp} behavior="padding">
+        <Block padding={[0, theme.sizes.base * 2]}>
+          <Text h2 bold>
+            Sign Up
+          </Text>
+          <Block middle>
+            <Input
+              email
+              error={hasErrors("email")}
+              label="Email"
+              style={[styles.input, hasErrors("email")]}
+              defaultValue={this.state.email}
+              onChangeText={text => this.setState({ email: text })}
+            />
+            <Input
+              error={hasErrors("username")}
+              label="Username"
+              style={[styles.input, hasErrors("username")]}
+              defaultValue={this.state.username}
+              onChangeText={text => this.setState({ username: text })}
+            />
+            <Input
+              secure
+              error={hasErrors("password")}
+              label="Password"
+              style={[styles.input, hasErrors("password")]}
+              defaultValue={this.state.password}
+              onChangeText={text => this.setState({ password: text })}
+            />
+
+            <Button gradient onPress={() => this.handleSignUp()}>
+              {loading ? (
+                <ActivityIndicator size="small" color="white" />
+              ) : (
+                <Text bold white center>
+                  Sign Up
+                </Text>
+              )}
+            </Button>
+            <Button onPress={() => navigation.navigate("Login")}>
+              <Text
+                gray
+                caption
+                center
+                style={{ textDecorationLine: "underline" }}
+              >
+                Back to Login
+              </Text>
+            </Button>
+          </Block>
+        </Block>
+      </KeyboardAvoidingView>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  SignUp: {
+    flex: 1,
+    justifyContent: "center"
+  },
+
+  input: {
+    borderRadius: 0,
+    borderWidth: 0,
+
+    borderBottomColor: theme.colors.gray2,
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  hasErrors: {
+    borderBottomColor: theme.colors.accent
+  }
+});
